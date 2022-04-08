0662ecf3d2d2d5eb5a16fb6910ff9aa1ccf11ff6
slslslrhfem <57289103+slslslrhfem@users.noreply.github.com>
2022-04-08 09:26:32 +0900

    main file and samples

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ import os
 os.environ["TF_FORCE_GPU_ALLOW_GROWTH"]="true"
 
 def main():
-    print(tf.__version__)
-    gpu_devices = tf.config.experimental.list_physical_devices("GPU")
-    for device in gpu_devices:
-        tf.config.experimental.set_memory_growth(device, True)
+
     if sys.argv[1] == 'train_classifier': # this learn 2 classifier model and save two model.
         classifiers = classifier_models()
         classifiers.label_classifier_train()
@@ -29,6 +26,15 @@ def main():
     elif sys.argv[1] == 'train_rnn':
         rnn_models = RNN_models()
         rnn_models.RNN_train()
+    elif sys.argv[1] == 'training':
+        classifiers = classifier_models()
+        classifiers.label_classifier_train()
+        classifiers.updown_classifier_train()
+        gan_models = GAN_models()
+        gan_models.training_gan()
+        rnn_models = RNN_models()
+        rnn_models.RNN_train()
+    
     else:
         print(sys.argv[1])
     
